f61e1e2ce95149c2fc51e2242176c6e97d22483b
Diwakar <hexstu009@sdettraining.com>
2016-04-05 10:16:04 -0400

    First Java Program with changes - HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,13 @@ package com.diwakar;
 
 class HelloWorld
 {
-	public static void main (String [] args)
+	public void sayHi ()
 	{
-		System.out.println("Hello World");
+		System.out.println("Hi from git to jenkins");
+	}
+
+	public static void main(String args[])
+	{
+		new Hello().sayHi();
 	}
 }
